e9d34aceab5a587eee0161d8685007ecba63931c
Saitama <najib.boukhari@condorcet.be>
2025-11-16 12:57:17 +0100

    3ème

---
+++ Exercices-ob-2/Comptebancaire.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel.Design;
+
+namespace Exercices_ob_2;
+
+//Propriété 
+public class Comptebancaire
+{
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+    public string Numerodecompte { get; set; }
+    private decimal Solde { get; set; }
+    
+    
+    //Constructeur 
+    public Comptebancaire(string numero, decimal soldeInitial, string nom, string prenom)
+    {
+        Numerodecompte = numero;
+        Nom = nom;
+        Prenom = prenom;
+        Solde = soldeInitial;
+        
+    }
+
+    public void deposer(decimal montant)
+
+    {
+        if (montant > 0)
+        {
+            Solde +=montant;
+        }
+    }
+
+    public bool Retirer(decimal montant)
+    {
+        if (montant > 0 && montant <= Solde)
+        {
+            Solde -= montant;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Le solde est insuffisant");
+             return false;
+        }
+    }
+
+    public void AfficherSolde()
+        {
+            //Console.WriteLine($"Solde de débart {soldeInitial}");
+            Console.WriteLine($"Mr {Nom} il vous reste sur votre compte le montant de {Solde} !");
+        }
+}
--- Exercices-ob-2/Program.cs
+++ Exercices-ob-2/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Exercices_ob_2;
+
+var client1 = new Comptebancaire(numero: "BE02 556 556 5676", 50, "Balieu", "Martin");
+
+client1.AfficherSolde();
+client1.deposer(15);
+client1.AfficherSolde();
+client1.Retirer(80);
+client1.AfficherSolde();
