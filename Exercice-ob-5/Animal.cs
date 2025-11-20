4d589a0e53297e513dd98ade1edd6b8d14431082
Saitama <najib.boukhari@condorcet.be>
2025-11-20 11:17:12 +0100

    5ème

--- Exercice-ob-5/Animal.cs
+++ Exercice-ob-5/Animal.cs
@@ -4,7 +4,7 @@ public class Animal
 {
     public string Nom { get; set; }
     public int Age { get; set; }
-    public string EspeceType {get; set; }
+    public string EspeceType { get; set; }
 
     public Animal(string nom, int age, string especeType)
     {
@@ -12,59 +12,61 @@ public class Animal
         Age = age;
         EspeceType = especeType;
     }
+
     public virtual void Fairebruit()
     {
         Console.WriteLine($"{Nom} l'animal fait du bruit");
     }
-    public class Chien : Animal
+
+    //public abstract void Fairebruit();
+    
+    
+    public class Chien : Animal 
     {
         public Chien(string nom, int age, string especeType) : base(nom, age, especeType)
         {
         }
 
-        public override void FaireDuBruit()
-        {
-            Console.WriteLine(" Woof! Woof!");
-            base.Fairebruit();
-        }
+        public override void Fairebruit()
+            {
+                Console.WriteLine(" Woof! Woof!");
+                base.Fairebruit();
+            }
     }
+    
 
     public class Chat : Animal
     {
-        public Chat(string nom, int age) : base(nom, age)
+        public Chat(string nom, int age, string especeType) : base(nom, age, especeType)
         {
         }
 
-        public override void FaireDuBruit()
-        {
-            Console.WriteLine(" Miaou! Miaou!");
-            base.Fairebruit();
+        public override void Fairebruit()
+            {
+                Console.WriteLine(" Miaou! Miaou!");
+                base.Fairebruit();
+            }
+
         }
-        
-    }
+    
 
     public class Oiseau : Animal
     {
-        public Oiseau(string nom, int age) : base(nom, age)
+        public Oiseau(string nom, int age, string especeType) : base(nom, age, especeType)
         {
         }
 
-        public override void FaireDuBruit()
-        {
-            Console.WriteLine(" Piou! Piou!");
-            base.Fairebruit();
+        public override void Fairebruit()
+            {
+                Console.WriteLine(" Piou! Piou!");
+                base.Fairebruit();
+            }
+
         }
-        
-    }
 
     public void Manger(string nourriture)
     {
-       Console.WriteLine($"{Nom}donner de la {nourriture}.");
+        Console.WriteLine($"{Nom} donner de la {nourriture}.");
     }
 
-    public virtual void FaireDuBruit()
-    {
-        throw new NotImplementedException();
-    }
 }
-
